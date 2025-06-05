441300e3825adf4d70d6316df7137cd1e086481e
Lamont <42653874+lamont-llp@users.noreply.github.com>
2025-06-05 12:58:06 +0200

    Add default ageGroup and month selection.

--- app/_components/AgeGroupSelect.jsx
+++ app/_components/AgeGroupSelect.jsx
@@ -1,35 +1,41 @@
-"use client"
+"use client";
 
-import React, {useEffect, useState} from 'react'
-import GlobalApi, {AgeGroup} from "../services/GlobalApi";
+import React, { useEffect, useState } from "react";
+import GlobalApi, { AgeGroup } from "../services/GlobalApi";
 
-function AgeGroupSelect({selectedAgeGroup}) {
-    const [ageGroups, setAgeGroups] = useState([])
+function AgeGroupSelect({ selectedAgeGroup }) {
+  const [ageGroups, setAgeGroups] = useState([]);
 
-    const GetAllAgeGroupsList = () => {
-        GlobalApi.GetAllAgeGroups().then((response) => {
-            setAgeGroups(response.data);
-        })
-    }
+  const GetAllAgeGroupsList = () => {
+    GlobalApi.GetAllAgeGroups().then((response) => {
+      setAgeGroups(response.data);
+    });
+  };
 
-    useEffect(() => {
-        GetAllAgeGroupsList()
-    }, [])
+  useEffect(() => {
+    GetAllAgeGroupsList();
+  }, []);
 
-    return (
-        <div>
-            <select
-                className='p-2 border rounded-lg'
-                onChange={(event) => {selectedAgeGroup(event.target.value)}}
-                defaultValue=""
-            >
-                <option value="" disabled>Select Age Group</option>
-                {ageGroups.map((item, index) => (
-                    <option key={index} value={item.group}>{item.group}</option>
-                ))}
-            </select>
-        </div>
-    )
+  return (
+    <div>
+      <select
+        className="p-2 border rounded-lg"
+        onChange={(event) => {
+          selectedAgeGroup(event.target.value);
+        }}
+        defaultValue="3-5yrs"
+      >
+        <option value="" disabled>
+          Select Age Group
+        </option>
+        {ageGroups.map((item, index) => (
+          <option key={index} value={item.group}>
+            {item.group}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 }
 
-export default AgeGroupSelect
+export default AgeGroupSelect;
--- app/_components/MonthSelection.jsx
+++ app/_components/MonthSelection.jsx
@@ -1,45 +1,49 @@
-"use client"
+"use client";
 
 import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-} from "@/components/ui/popover"
-import { Calendar } from "@/components/ui/calendar"
-import {Button} from "../../components/ui/button";
-import {CalendarDays} from "lucide-react";
-import {addMonths} from 'date-fns';
-import {useState} from "react";
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Calendar } from "@/components/ui/calendar";
+import { Button } from "../../components/ui/button";
+import { CalendarDays } from "lucide-react";
+import { addMonths } from "date-fns";
+import { useState } from "react";
 import moment from "moment";
 
-function MonthSelection({selectedMonth}) {
-    const today = new Date();
-    const nextMonths = addMonths(new Date(), 0);
-    const [month, setMonth] = useState(nextMonths);
+function MonthSelection({ selectedMonth }) {
+  const today = new Date();
+  const nextMonths = addMonths(new Date(), 0);
+  const [month, setMonth] = useState(nextMonths);
 
-    return (
-        <div>
-            <Popover>
-                <PopoverTrigger asChild>
-                    <Button variant="outline" className="flex gap-2 items-center text-slate-500">
-                        <CalendarDays className="h-5 w-5"/>
-                        {moment(month).format('MMM YYYY')}
-                    </Button>
-                </PopoverTrigger>
-                <PopoverContent>
-                    <Calendar
-                        mode="single"
-                        selected={month}
-                        onSelect={(date) => {
-                            setMonth(date);
-                            selectedMonth(date);
-                        }}
-                        className="rounded-md border"
-                    />
-                </PopoverContent>
-            </Popover>
-        </div>
-    )
+  return (
+    <div>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            className="flex gap-2 items-center text-slate-500"
+          >
+            <CalendarDays className="h-5 w-5" />
+            {moment(month).format("MMM YYYY")}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent>
+          <Calendar
+            mode="single"
+            defaultMonth={month}
+            selected={month}
+            onSelect={(date) => {
+              setMonth(date);
+              selectedMonth(date);
+            }}
+            className="rounded-md border"
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
 }
 
-export default MonthSelection
+export default MonthSelection;
