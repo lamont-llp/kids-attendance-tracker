749b286158030870ef36d7368cbffa6451621b06
Lamont <42653874+lamont-llp@users.noreply.github.com>
2025-06-05 17:54:06 +0000

    Add light/dark mode toggle to header.

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -3,13 +3,14 @@
 import React from 'react'
 import {useKindeBrowserClient} from "@kinde-oss/kinde-auth-nextjs";
 import Image from "next/image";
+import { ModeToggle } from '@/components/ModeToggle';
 
 function Header() {
     const {user} = useKindeBrowserClient();
     return (
         <div className='p-4 shadow-sm border flex justify-between'>
             <div>
-
+                <ModeToggle/>
             </div>
             { user && user.picture ?(
                 <div>
