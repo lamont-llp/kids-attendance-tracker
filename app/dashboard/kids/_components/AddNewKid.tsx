9180b26dc6e31b772401aadaf2c1b2c05a4f3083
Lamont <42653874+lamont-llp@users.noreply.github.com>
2025-10-23 08:21:57 +0200

    fix: convert kidAge to a number for AgeGroupSelect component

--- app/dashboard/kids/_components/AddNewKid.tsx
+++ app/dashboard/kids/_components/AddNewKid.tsx
@@ -119,7 +119,7 @@ function AddNewKid({ refreshData }: AddNewKidProps) {
                 <FormItem className="flex flex-col py-2">
                   <FormLabel>Age Group (automatically determined)</FormLabel>
                   <FormControl>
-                    <AgeGroupSelect age={kidAge} selectedAgeGroup={setAgeGroup} />
+                    <AgeGroupSelect age={Number(kidAge)} selectedAgeGroup={setAgeGroup} />
                   </FormControl>
                 </FormItem>
                 <FormItem className="py-2">
